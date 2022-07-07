b4ef34bcc544d3084575fe483bcf069dd6b39c0c
Kirtan <kirtan.gadhiya@bacancy.com>
2022-07-07 15:38:15 +0530

    Clean the Code

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
 const verifyToken = require("../middleware/verifyToken");
 const { uservalidation } = require("../controller/user.validation");
 const upload = require("../utils/fileUpload");
